71e2ade960c9650721d37fb6da9ce878d4021789
grus <gasperus@gmail.com>
2019-11-07 12:02:16 +0100

    Add main component, homepage, and styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import {configureStore} from "../store";
 import {BrowserRouter as Router} from "react-router-dom";
 
 import Navbar from "./Navbar";
+import Main from "./Main";
 
 const store = configureStore()
 
@@ -12,6 +13,7 @@ const App = () => (
     <Router>
       <div className="onboarding">
         <Navbar />
+        <Main />
       </div>
     </Router>
   </Provider>
